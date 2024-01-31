cbf516f2ff629a22a4bd44130e2995f3f03fa116
DdingJae418 <ssy008567@naver.com>
2024-01-31 19:11:36 +0900

    first commit

---
+++ 30617.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+int main(void) {
+    int leftNode = 0, rightNode = 0, leftDir, rightDir;
+    int funRate = 0;
+    int runTime;
+
+    cin>>runTime;
+
+    for(int i=0; i<runTime; i++){
+        cin>>leftDir>>rightDir;
+        
+    }
+
+    return 0;
+}
+
+void calculateFunRate()
